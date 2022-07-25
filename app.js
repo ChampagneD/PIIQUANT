18dc7674d8de23327d5a100cd88f7ed63cada3ec
Champagne <efingbv@gmail.com>
2022-07-25 17:11:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+app.use('/api/auth', userRoutes);
+
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
